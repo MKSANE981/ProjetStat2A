9e12cdb9412f4827971064a9a0f66e753edb0585
MKSANE981 <mansoursanneh98@gmail.com>
2024-02-22 21:40:59 +0100

    ...

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+from PIL import Image
+from st_pages import Page, show_pages, add_page_title
+
+logo = Image.open('Ensai-logo.png')
+st.set_page_config(page_title="Scoring credit", page_icon=logo, layout="wide")
+add_page_title("Scoring Credit")
+
+show_pages(
+    [
+        Page("main.py", "Activité bancaire", "📈"),
+        Page("DashBoard.py", "Global Information", "📉"),
+        Page("ClientInfo.py", "Client Information", "👨‍✈️️"),
+        Page("Metadata.py", "Explication des données", ":bar_chart:"),
+
+    ]
+)
+st.markdown("""<style>.big-font {font-size: 24px; text-align:justify;} .huge {font-size: 40px;font-weight: bold;} .color{color:green;font-size: 30px;} </style>
+        """, unsafe_allow_html=True)
